3c8298ed153f2f942ff7c89eb48bee917b025b2d
Renlord <me@renlord.com>
2020-01-15 01:03:39 -0800

    unknown source feature add Wi-Fi timeout, usb deny, fingerprint disable, PIN scrambling (toggle) and code clean up
    Signed-off-by: June <june@eridan.me>
    
    add Wi-Fi timeout feature add fingerprint disable
    
    add a toggle for disabling fingerprint unlock
    
    Ported from 11. In 12 fingerprint is handled by the new biometric API and so technically in 12 this is not just disabling fingerprint unlock but all biometrics. This does not affect the Pixel 4 generation of devices with face scanning however as they were released on Q and so use the legacy face API. No frameworks/base changes are needed for 12 in this case other than migration which can be done in another commit.
    
    add auto-reboot deny_new_usb and pin setting
    
    add deny_new_usb setting and pin setting
    
    Based on original patch by Daniel Micay <danielmicay@gmail.com>.
    
    add PIN scrambling setting
    
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>
    Change-Id: Ib2f60f6d990fd210cdbb4d109fbe2faa50f9b91d
    
    code clean up
    
    Change-Id: Ia09dfd67bcfb8334439bfb9dab419235b182e297
    
    h
    
    UserManager unknown source app installation restrictions

--- src/com/android/settings/security/SecuritySettings.java
+++ src/com/android/settings/security/SecuritySettings.java
@@ -122,6 +122,9 @@ public class SecuritySettings extends DashboardFragment {
         securityPreferenceControllers.add(new CombinedBiometricStatusPreferenceController(
                 context, lifecycle));
         securityPreferenceControllers.add(new ChangeScreenLockPreferenceController(context, host));
+        securityPreferenceControllers.add(new PinScramblePreferenceController(context));
+        securityPreferenceControllers.add(new AutoRebootPreferenceController(context));
+        securityPreferenceControllers.add(new DenyNewUsbPreferenceController(context));
         controllers.add(new PreferenceCategoryController(context, SECURITY_CATEGORY)
                 .setChildren(securityPreferenceControllers));
         controllers.addAll(securityPreferenceControllers);
