0c6d51e44d87a31a58a89eb4c1be0fc4a52afda5
Alex Cruz <mazdarider23@gmail.com>
2020-07-19 16:26:53 +0530

    Anatolia: add method to get Theme Accent
    Change-Id: Ic19a8f4bcf55b2e7202a0f527f76295aafb60dfe
    Signed-off-by: Escobar1945 <candaroglu197878@gmail.com>

--- src/com/epic/Anatolia.java
+++ src/com/epic/Anatolia.java
@@ -21,9 +21,12 @@ package com.epic;
 import com.android.internal.logging.nano.MetricsProto;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.KeyEvent;
 import android.view.Surface;
 import android.preference.Preference;
 import com.android.settings.R;
@@ -42,5 +45,11 @@ public class Anatolia extends SettingsPreferenceFragment {
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.EPIC;
+    }
+
+    public static int getThemeAccentColor (final Context context) {
+        final TypedValue value = new TypedValue ();
+        context.getTheme ().resolveAttribute (android.R.attr.colorAccent, value, true);
+        return value.data;
     }
 }
