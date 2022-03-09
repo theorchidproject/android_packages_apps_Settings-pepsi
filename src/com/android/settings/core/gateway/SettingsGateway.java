fd676aebae30db6d6d734a22734f9ae4e183dffc
xyzuan <xyzuannihboss@gmail.com>
2022-03-09 16:56:30 +0800

    OrchidOs Fragment Settings: Add grimoire fragments
    add grimoire fragments to settings need to add proto change in frameworks base. CUSTOM_SETTINGS
    
    Co-authored-by: maxwen <max.weninger@gmail.com>
    Signed-off-by: dlwlrma123 <alexfinhart@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -167,6 +167,7 @@ import com.android.settings.wifi.details.WifiNetworkDetailsFragment;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
+import com.arcana.grimoire.Dashboard;
 
 import com.sakura.settings.SakuraSettings;
 
@@ -327,6 +328,7 @@ public class SettingsGateway {
             AlarmsAndRemindersDetails.class.getName(),
             MediaManagementAppsDetails.class.getName(),
             SakuraSettings.class.getName()
+            Dashboard.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -368,5 +370,6 @@ public class SettingsGateway {
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
             Settings.SakuraSettingsActivity.class.getName(),
+            Settings.grimoireActivity.class.getName(),
     };
 }
