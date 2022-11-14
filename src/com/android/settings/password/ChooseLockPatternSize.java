CrystalOS-Temp/packages_apps_Settings

Fix strings and MetricsEvent
Change-Id: I16aa8e5be271abddaaca1ce933107a640b460024

src/com/android/settings/password/ChooseLockPatternSize.java
@@ -146,7 +146,7 @@ public class ChooseLockPatternSize extends SettingsActivity {
 
         @Override
         public int getMetricsCategory() {
-            return MetricsEvent.CHERISH_SETTINGS;
+            return MetricsEvent.CRYSTAL;
         }
     }
 }

theorchidproject/android_packages_apps_Settings-pepsi

src/com/android/settings/password/ChooseLockPatternSize.java
@@ -147,7 +147,7 @@ public class ChooseLockPatternSize extends SettingsActivity {
 
         @Override
         public int getMetricsCategory() {
-            return LineageMetricsLogger.CHOOSE_LOCK_PATTERN_SIZE;
+            return MetricsEvent.CUSTOM_SETTINGS;
         }
     }
 }